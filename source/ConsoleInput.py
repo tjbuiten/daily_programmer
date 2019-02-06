ffdef45a3a30b04fe6329006b7d83da60e7d097a
tjbuiten <timobuitenhuis@gmail.com>
2019-02-06 11:53:26 +0100

    Added functionality to write the input to a file.

--- source/ConsoleInput.py
+++ source/ConsoleInput.py
@@ -1,3 +1,7 @@
 print("Please state your name, age and reddit username seperated by a comma and an empty space (, ) :")
 name, age, username = input().split(', ')
+
 print("your name is %(n)s, you are %(a)s years old, and your username is %(u)s" % {'n': name, 'a': age, 'u': username})
+
+file = open("userData.txt", "a")
+file.write("your name is %(n)s, you are %(a)s years old, and your username is %(u)s" % {'n': name, 'a': age, 'u': username})
