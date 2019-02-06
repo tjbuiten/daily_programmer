6180e671aba929a861bbc50b5e2bbb779e484dc6
tjbuiten <timobuitenhuis@gmail.com>
2019-02-06 12:36:16 +0100

    Merged one print and input statement.

--- source/ConsoleInput.py
+++ source/ConsoleInput.py
@@ -1,6 +1,4 @@
-print("Please state your name, age and reddit username seperated by a comma and an empty space (, ) :")
-name, age, username = input().split(', ')
-
+name, age, username = input("Please state your name, age and reddit username seperated by a comma and an empty space (, ) :\n").split(', ')
 print("your name is %(n)s, you are %(a)s years old, and your username is %(u)s" % {'n': name, 'a': age, 'u': username})
 
 file = open("userData.txt", "a")
